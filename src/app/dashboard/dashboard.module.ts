cd62ac644a061b22790c366e788b3dcb46fb7e32
Claudiomarcosep <claudiomarcosep@gmail.com>
2021-06-02 22:05:14 -0300

    Front-end first version

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,23 @@
+import { HttpClientModule} from '@angular/common/http'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { StockCardComponent } from './stock-card/stock-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardPageComponent,
+    StockCardComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [
+
+  ]
+
+})
+export class DashboardModule { }
---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,13 @@
+class Stock {
+
+  id: number;
+  name: string;
+  price: number;
+  minimum: number;
+  exclusiveMinimum: boolean;
+  variation: number;
+  date: string;
+  stock: Stock;
+}
+
+export default Stock;
